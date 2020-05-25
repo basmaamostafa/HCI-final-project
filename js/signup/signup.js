31b12650c80374759894a633db0c317e031b839a
basmaamostafa <57597830+basmaamostafa@users.noreply.github.com>
2020-05-25 19:30:49 +0200

    email

--- js/signup/signup.js
+++ js/signup/signup.js
@@ -11,4 +11,30 @@ function validateForm() {
       alert("Please enter only letters in this field.");
     }
   }
+
+  var email = document.forms["myForm"]["mail"].value;
+  if (email == "") {
+    // alert("Name must be filled out");
+    var e = document.getElementById("nameError");
+    e.innerHTML = "Name must be filled out";
+    return false;
+  } else {
+    var emailTest = email.search(/[a-zA-Z]/g);
+    if (emailTest == -1) {
+      alert("Please enter valid email in this field.");
+    }
+  }
+
+  var email = document.forms["myForm"]["fnapassme"].value;
+  if (email == "") {
+    // alert("Name must be filled out");
+    var e = document.getElementById("nameError");
+    e.innerHTML = "Name must be filled out";
+    return false;
+  } else {
+    var emailTest = email.search(/[a-zA-Z]/g);
+    if (emailTest == -1) {
+      alert("Please enter valid email in this field.");
+    }
+  }
 }
