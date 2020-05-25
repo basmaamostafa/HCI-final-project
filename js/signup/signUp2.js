5ace84610f57b7e1a73d0238edad509f8ffa73ee
Mohamed Abdelhady Elshamy <62076892+ABDELHADY154@users.noreply.github.com>
2020-05-25 20:24:24 +0200

    HADY

--- js/signup/signUp2.js
+++ js/signup/signUp2.js
@@ -1,11 +1,13 @@
 function session() {
+  var printBio = document.getElementById("bio");
+
   var signupBio = document.getElementById("signupBio").value;
   if (typeof Storage !== "undefined") {
     if (signupBio) {
       // Store
       sessionStorage.setItem("BIO", signupBio);
       // Retrieve
-      document.getElementById("bio").innerHTML = sessionStorage.getItem("BIO");
+      printBio.innerHTML = sessionStorage.getItem("BIO");
     } else {
       document.getElementById("bio").innerHTML =
         "Sorry, your browser does not support Web Storage...";
@@ -16,3 +18,4 @@ function session() {
     //   n.innerHTML = sessionStorage.getItem("Bio");
   }
 }
+// var bio = printBio.innerHTML;
