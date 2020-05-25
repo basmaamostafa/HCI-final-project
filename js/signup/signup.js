58de9469fed208125d4e97b54406e34dd4026592
basmaamostafa <57597830+basmaamostafa@users.noreply.github.com>
2020-05-25 19:39:19 +0200

    mail

--- js/signup/signup.js
+++ js/signup/signup.js
@@ -1,15 +1,30 @@
 function validateForm() {
   var fullName = document.forms["myForm"]["fname"].value;
-  var e = document.getElementById("nameError");
+  var n = document.getElementById("nameError");
 
   if (fullName == "") {
     // alert("Name must be filled out");
-    e.innerHTML = "Name must be filled out";
+    n.innerHTML = "Name must be filled out";
     return false;
   } else {
     var nameTest = fullName.search(/^[A-Za-z]+$/);
     if (nameTest == -1) {
-      e.innerHTML = "enter letters only";
+      n.innerHTML = "Please enter letters only";
+      return false;
+    }
+  }
+
+  var email = document.forms["myForm"]["mail"].value;
+  var e = document.getElementById("mailError");
+
+  if (email == "") {
+    // alert("Name must be filled out");
+    e.innerHTML = "E-Mail must be filled out";
+    return false;
+  } else {
+    var emailTest = email.search(/^[A-Za-z]+$/);
+    if (emailTest == -1) {
+      e.innerHTML = "Please enter a valid email";
       return false;
     }
   }
