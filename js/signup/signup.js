5f3b0d7016858cde2facc43534ffea1768d034ef
Mohamed Abdelhady Elshamy <62076892+ABDELHADY154@users.noreply.github.com>
2020-05-25 19:32:05 +0200

    name update

--- js/signup/signup.js
+++ js/signup/signup.js
@@ -1,14 +1,16 @@
 function validateForm() {
   var fullName = document.forms["myForm"]["fname"].value;
+  var e = document.getElementById("nameError");
+
   if (fullName == "") {
     // alert("Name must be filled out");
-    var e = document.getElementById("nameError");
     e.innerHTML = "Name must be filled out";
     return false;
   } else {
     var nameTest = fullName.search(/^[A-Za-z]+$/);
     if (nameTest == -1) {
-      alert("Please enter only letters in this field.");
+      e.innerHTML = "enter letters only";
+      return false;
     }
   }
 }
