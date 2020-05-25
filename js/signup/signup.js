8c5a17b0e957c06659793da0bbaace74fd663a5d
basmaamostafa <57597830+basmaamostafa@users.noreply.github.com>
2020-05-25 21:35:03 +0200

    signup2

--- js/signup/signup.js
+++ js/signup/signup.js
@@ -53,11 +53,34 @@ function validateForm() {
   var cp = document.getElementById("cpassError");
 
   if (confirmPassword == "") {
-    // alert("Name must be filled out");
     cp.innerHTML = "Password must be filled out";
     return false;
   } else if (confirmPassword !== password) {
     cp.innerHTML = "Please enter a the same password";
     return false;
   }
+
+  var termsofuse = document.getElementById("check").checked;
+  var checkerror = document.getElementById("checkError");
+  if (!termsofuse) {
+    checkerror.innerHTML = "Please accept terms of use";
+    return false;
+  }
+
+  //
+  var bio = document.forms["myForm"]["biotxt"].value;
+  var bioP = document.getElementById("bioError");
+
+  if (bio == "") {
+    bioP.innerHTML = "Name must be filled out";
+    return false;
+  } else {
+    var bioTest = bio.search("/^[A-Za-z]+$/");
+    if (bioTest == -1) {
+      bioP.innerHTML = "Please enter letters only";
+      return false;
+    }
+  }
 }
+var test = document.getElementById("test");
+test.innerHTML = "Please enter letters only";
