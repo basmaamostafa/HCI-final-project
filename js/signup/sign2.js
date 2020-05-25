fbc47ccb644f7322e05fa595606850b33620ce70
basmaamostafa <57597830+basmaamostafa@users.noreply.github.com>
2020-05-26 01:50:41 +0200

    skills

--- js/signup/sign2.js
+++ js/signup/sign2.js
@@ -1,17 +1,37 @@
 function validateForm() {
+  var edu = document.forms["myForm"]["edutxt"].value;
+  var eduP = document.getElementById("eduError");
+
+  if (edu == "") {
+    eduP.innerHTML = "Education level must be filled out";
+    return false;
+  }
+
   var bio = document.forms["myForm"]["biotxt"].value;
   var bioP = document.getElementById("bioError");
-
   if (bio == "") {
-    bioP.innerHTML = "Name must be filled out";
+    bioP.innerHTML = "Bio must be filled out";
     return false;
-  } else {
-    var bioTest = bio.search("/^[A-Za-z]+$/");
-    if (bioTest == -1) {
-      bioP.innerHTML = "Please enter letters only";
-      return false;
-    }
+  }
+
+  var skill = document.forms["myForm"]["skill"].value;
+  var skillP = document.getElementById("skillError");
+  if (skill == "") {
+    skillP.innerHTML = "Skill/Tolls must be filled out";
+    return false;
+  }
+
+  var college = document.forms["myForm"]["college"].value;
+  var collegep = document.getElementById("collegeError");
+  if (college == "") {
+    collegep.innerHTML = "College/University must be filled out";
+    return false;
+  }
+
+  var date = document.forms["myForm"]["date"].value;
+  var datep = document.getElementById("dateError");
+  if (date == "") {
+    datep.innerHTML = "Birthday date must be filled out";
+    return false;
   }
 }
-var test = document.getElementById("test");
-test.innerHTML = "Please enter letters only";
--- js/signup/signup.js
+++ js/signup/signup.js
@@ -66,21 +66,4 @@ function validateForm() {
     checkerror.innerHTML = "Please accept terms of use";
     return false;
   }
-
-  //
-  var bio = document.forms["myForm"]["biotxt"].value;
-  var bioP = document.getElementById("bioError");
-
-  if (bio == "") {
-    bioP.innerHTML = "Name must be filled out";
-    return false;
-  } else {
-    var bioTest = bio.search("/^[A-Za-z]+$/");
-    if (bioTest == -1) {
-      bioP.innerHTML = "Please enter letters only";
-      return false;
-    }
-  }
 }
-var test = document.getElementById("test");
-test.innerHTML = "Please enter letters only";
