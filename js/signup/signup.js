3f3d928d2b2c1a152d8c9bd97ed06fcec3a4a147
basmaamostafa <57597830+basmaamostafa@users.noreply.github.com>
2020-05-25 20:06:00 +0200

    confirmpass

--- js/signup/signup.js
+++ js/signup/signup.js
@@ -50,18 +50,15 @@ function validateForm() {
   }
 
   var confirmPassword = document.forms["myForm"]["confirm"].value;
-  var cp = document.getElementById("passError");
+  var cp = document.getElementById("cpassError");
 
   if (confirmPassword == "") {
     // alert("Name must be filled out");
     cp.innerHTML = "Password must be filled out";
     return false;
-  } else {
-    var conpassTest == passTest);
-
-    if (conpassTest == -1) {
-      cp.innerHTML = "Please enter a the same password";
+  } else (confirm == pass){
+          cp.innerHTML = "Please enter a the same password";
       return false;
-    }
   }
+
 }
