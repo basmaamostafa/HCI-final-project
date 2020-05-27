a386cdc362d16d79d6ae374216f27b10001845ae
Mohamed Abdelhady Elshamy <62076892+ABDELHADY154@users.noreply.github.com>
2020-05-27 10:13:44 +0200

    ..

--- js/signup/signup.js
+++ js/signup/signup.js
@@ -10,7 +10,7 @@ function validateForm() {
   var cp = document.getElementById("cpassError");
   var termsofuse = document.getElementById("check").checked;
   var checkerror = document.getElementById("checkError");
-  var nameTest = fullName.search(/^[A-Za-z]+$/);
+  var nameTest = fullName.search(/^[a-z,',-]+(\s)[a-z,',-]+$/i);
   var emailTest = email.search(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
   var passTest = /^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*])[\w!@#$%^&*]{8,}$/.test(
     password
