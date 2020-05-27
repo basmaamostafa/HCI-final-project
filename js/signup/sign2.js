48e0dc12cd606cd686a83b2f06f232171b519370
Mohamed Abdelhady Elshamy <62076892+ABDELHADY154@users.noreply.github.com>
2020-05-27 08:41:21 +0200

    123123

--- js/signup/sign2.js
+++ js/signup/sign2.js
@@ -1,37 +1,32 @@
 function validateForm() {
   var edu = document.forms["myForm"]["edutxt"].value;
   var eduP = document.getElementById("eduError");
+  var bio = document.forms["myForm"]["biotxt"].value;
+  var bioP = document.getElementById("bioError");
+  var skill = document.getElementById("skillSpan");
+  // document.forms["myForm"]["skill"].value;
+  var skillP = document.getElementById("skillError");
+  var college = document.forms["myForm"]["college"].value;
+  var collegep = document.getElementById("collegeError");
+  var date = document.forms["myForm"]["date"].value;
+  var datep = document.getElementById("dateError");
 
   if (edu == "") {
     eduP.innerHTML = "Education level must be filled out";
     return false;
-  }
-
-  var bio = document.forms["myForm"]["biotxt"].value;
-  var bioP = document.getElementById("bioError");
-  if (bio == "") {
+  } else if (bio == "") {
     bioP.innerHTML = "Bio must be filled out";
     return false;
-  }
-
-  var skill = document.forms["myForm"]["skill"].value;
-  var skillP = document.getElementById("skillError");
-  if (!skill) {
+  } else if (!skill) {
     skillP.innerHTML = "Skill/Tolls must be filled out";
     return false;
-  }
-
-  var college = document.forms["myForm"]["college"].value;
-  var collegep = document.getElementById("collegeError");
-  if (college == "") {
+  } else if (college == "") {
     collegep.innerHTML = "College/University must be filled out";
     return false;
-  }
-
-  var date = document.forms["myForm"]["date"].value;
-  var datep = document.getElementById("dateError");
-  if (date == "") {
+  } else if (!date) {
     datep.innerHTML = "Birthday date must be filled out";
     return false;
+  } else {
+    return true;
   }
 }
