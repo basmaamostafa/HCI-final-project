a8feeffd513a2551269dd657e56a08034d500ed8
basmaamostafa <57597830+basmaamostafa@users.noreply.github.com>
2020-05-29 11:28:00 +0200

    img2

--- js/signup/img.js
+++ js/signup/img.js
@@ -1,36 +1,17 @@
-// preview Image Before Upload
-function previewImageBeforeUpload() {
-  var element = $(".preview-img-upload");
-  var reader;
-
-  function readURL(input, selector) {
-    if (input.files && input.files[0]) {
-      reader = new FileReader();
-      reader.onload = function (e) {
-        selector
-          .parents(".preview-img-upload")
-          .addClass("active")
-          .find(".preview-img")
-          .attr("src", e.target.result);
-      };
-      reader.readAsDataURL(input.files[0]);
-    }
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      $("#imagePreview").css(
+        "background-image",
+        "url(" + e.target.result + ")"
+      );
+      $("#imagePreview").hide();
+      $("#imagePreview").fadeIn(650);
+    };
+    reader.readAsDataURL(input.files[0]);
   }
-
-  function removeImage() {
-    element.find(".remove-image").click(function () {
-      $(this)
-        .parents(".preview-img-upload")
-        .removeClass("active")
-        .find(".preview-img")
-        .attr("src", "");
-      $(this).parents(".preview-img-upload").find("input").val("");
-    });
-  }
-  removeImage();
-
-  $(document.body).on("change", ".preview-img-upload input", function () {
-    readURL(this, $(this));
-  });
 }
-previewImageBeforeUpload();
+$("#imageUpload").change(function () {
+  readURL(this);
+});
